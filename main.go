49590ba8f432057c92de7db186dfb8384c7c7223
Marco Josefsen <majos6959@oslomet.no>
2024-12-03 17:24:26 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	sess, err := discordgo.New("Bot ...")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sess.AddHandlerOnce(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+
+		if m.Content == "Hello" {
+			s.ChannelMessageSend(m.ChannelID, "Hello, World!")
+		}
+	})
+
+	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = sess.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sess.Close()
+
+	fmt.Println("Bot is online!")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+}
