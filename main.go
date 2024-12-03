9ab2183181de9f0f245a6f16ad76e066fb96f402
Marco Josefsen <majos6959@oslomet.no>
2024-12-03 18:53:02 +0100

    Seperated main functions to sub functions, added basic file handler

--- main.go
+++ main.go
@@ -10,34 +10,56 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-
-	sess, err := discordgo.New("Bot ...")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	sess.AddHandlerOnce(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		if m.Author.ID == s.State.User.ID {
-			return
-		}
-
-		if m.Content == "Hello" {
-			s.ChannelMessageSend(m.ChannelID, "Hello, World!")
-		}
-	})
-
-	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
-
-	err = sess.Open()
+	token := "your-bot-token"
+	sess, err := initializeBot(token)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer sess.Close()
 
+	sess.AddHandler(handleMessage)
+
+	err = sess.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("Bot is online!")
 
+	// Wait for termination signal
 	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
+	fmt.Println("Bot shutting down.")
+}
+
+func initializeBot(token string) (*discordgo.Session, error) {
+	sess, err := discordgo.New("Bot " + token)
+	if err != nil {
+		return nil, err
+	}
+
+	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+	return sess, nil
+}
+
+func handleMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if len(m.Attachments) > 0 {
+		handleFiles(s, m)
+	} else if m.Content == "Hello" {
+		s.ChannelMessageSend(m.ChannelID, "Hello, World!")
+	}
+}
+
+func handleFiles(s *discordgo.Session, m *discordgo.MessageCreate) {
+	for _, attachment := range m.Attachments {
+		if attachment.Filename == "example.txt" {
+			channelID := "your-target-channel-id"
+			s.ChannelMessageSend(channelID, m.Content)
+		}
+	}
 }
