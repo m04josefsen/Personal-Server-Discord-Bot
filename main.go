8458dedfbee25f77487a0889d2865585d1a38c70
Marco Josefsen <majos6959@oslomet.no>
2024-12-04 21:09:58 +0100

    Added functions so bot sends message every 5 minutes (mostly becuase it is locally run)

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"os/signal"
 	"strings"
 	"syscall"
+	"time"
 )
 
 func main() {
@@ -28,7 +29,11 @@ func main() {
 
 	fmt.Println("Bot is online!")
 
+	// go makes it run concurrently with the rest of the code
+	go sendPeriodicMessages(sess)
+
 	// Wait for termination signal
+	// TODO: hva gjør den?
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
@@ -46,14 +51,13 @@ func initializeBot(token string) (*discordgo.Session, error) {
 }
 
 func handleMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// If the bot sends a message
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 
 	if len(m.Attachments) > 0 {
 		handleFiles(s, m)
-	} else if m.Content == "Hello" {
-		s.ChannelMessageSend(m.ChannelID, "Hello, World!")
 	}
 }
 
@@ -61,10 +65,22 @@ func handleFiles(s *discordgo.Session, m *discordgo.MessageCreate) {
 	fmt.Println("in handle files")
 
 	subjectToChannel := map[string]string{
-		"adse1310": "1",
+		// First year
+		// First semester
+		"data1100": "1313886461480927242",
+		"dape1300": "1313886404949966898",
+		"dape1400": "1313886439766884374",
+
+		// Second semester
+		"adse1310": "1313564389097603153",
+		"data1500": "1313886488991367188",
+		"data1700": "1313886509652377620",
+
+		// Second year
+		// Third semester
 		"adse2100": "1313564569058279506",
-		"dafe2200": "2",
-		"dats2300": "3",
+		"dafe2200": "1313564534132310047",
+		"dats2300": "1313564591740944394",
 	}
 
 	for _, attachment := range m.Attachments {
@@ -73,7 +89,9 @@ func handleFiles(s *discordgo.Session, m *discordgo.MessageCreate) {
 		lowerFilename := strings.ToLower(attachment.Filename)
 		fmt.Println(lowerFilename)
 
-		if strings.HasPrefix(lowerFilename, "adse") || strings.HasPrefix(lowerFilename, "dafe") || strings.HasPrefix(lowerFilename, "dats") {
+		// TODO: må kunne forkorte denne
+		// For Documents under OsloMet category
+		if strings.HasPrefix(lowerFilename, "adse") || strings.HasPrefix(lowerFilename, "dafe") || strings.HasPrefix(lowerFilename, "dats") || strings.HasPrefix(lowerFilename, "data") || strings.HasPrefix(lowerFilename, "dape") {
 			channelID := lowerFilename[:8]
 			fmt.Println("Extracted channelID:", channelID)
 
@@ -88,7 +106,7 @@ func handleFiles(s *discordgo.Session, m *discordgo.MessageCreate) {
 			if strings.HasSuffix(lowerFilename, ".pdf") {
 
 				// Forward the PDF
-				resp, err := http.Get(attachment.URL) // Download the PDF
+				resp, err := http.Get(attachment.URL)
 				if err != nil {
 					log.Println("Error downloading PDF:", err)
 					continue
@@ -96,15 +114,27 @@ func handleFiles(s *discordgo.Session, m *discordgo.MessageCreate) {
 				defer resp.Body.Close()
 
 				// Send the PDF to the target channel
-				_, err = s.ChannelFileSend(actualChannelID, attachment.Filename, resp.Body) // Corrected here
+				_, err = s.ChannelFileSend(actualChannelID, attachment.Filename, resp.Body)
 				if err != nil {
 					log.Println("Error forwarding PDF:", err)
 				} else {
 					log.Println("PDF forwarded successfully:", attachment.Filename)
 				}
-
-				fmt.Println("At the end of .pdf if")
 			}
+		}
+	}
+}
+
+func sendPeriodicMessages(s *discordgo.Session) {
+	channelID := "1313564463579791450"
+
+	ticker := time.NewTicker(5 * time.Minute)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		_, err := s.ChannelMessageSend(channelID, "OH MAH GAH!!")
+		if err != nil {
+			log.Println("Failed to send periodic message:", err)
 		}
 	}
 }
